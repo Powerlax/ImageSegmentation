74935eddc106cd6a20684bbc752a8d49d0096c41
Powerlax <raovishruth@gmail.com>
2023-08-03 16:53:24 -0700

    Added predicter

---
+++ predict.py
@@ -0,0 +1,15 @@
+import tensorflow as tf
+import helper
+    
+model = tf.keras.models.load_model("pets")
+
+while True:
+  url = input("Please enter an image url:")
+  try:
+    image = tf.keras.utils.get_file(origin=url)
+    image = tf.keras.utils.load_img(image)
+    break
+  except:
+    print("That is not a valid link")
+
+helper.show_predictions(url, model)
